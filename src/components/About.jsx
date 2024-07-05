04f72f318758f98385eedb7f60d99465324c4e77
Andre Lopes <andre@Andres-MacBook-Air.local>
2024-07-04 23:10:37 -0700

    here we go again

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+          With over a decade of experience, my core competencies revolve around creating captivating digital experiences and robust web architectures. At the helm of Alldrops Digital, my mission is to harness the power of cutting-edge technologies to deliver innovative solutions. My expertise in UI/UX design aligns with my company's goal of crafting interfaces that resonate with users and foster engagement.
+
+          My tenure at TELUS Digital has been marked by leading dynamic teams in the design and execution of marketing and checkout pages that enhance the customer journey. Utilizing JavaScript, HTML5, and CSS3, I have been instrumental in mentoring and educating developers, fostering an environment of continuous learning and improvement. Our efforts have culminated in scalable, reusable components that have significantly benefitted TELUS and its affiliates.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
